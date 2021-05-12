2d6e687f7e56acb9cb79378bbe24d19c184cc255
haliant <haliantms@gmail.com>
2021-05-12 22:07:57 +0300

    added drag and change width

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Paper } from '@material-ui/core';
 import { SearchState, IntegratedFiltering, SortingState, IntegratedSorting } from '@devexpress/dx-react-grid';
-import { Grid, Table, TableHeaderRow, SearchPanel, Toolbar } from '@devexpress/dx-react-grid-material-ui';
+import {
+	Grid,
+	Table,
+	TableHeaderRow,
+	SearchPanel,
+	Toolbar,
+	DragDropProvider,
+	TableColumnReordering,
+	TableColumnResizing,
+} from '@devexpress/dx-react-grid-material-ui';
 
 import { updatePostsRequest } from '../redux/ducks/posts';
 import { IpostsList } from '../redux/ducks/posts';
 import { RootState } from '../redux';
 
 const Posts: React.FC = () => {
-	const columns = [
+	const [columns] = useState([
 		{ name: 'id', title: 'ID' },
 		{ name: 'title', title: 'Title' },
 		{ name: 'body', title: 'Body' },
-	];
+	]);
 
 	const { postsList }: IpostsList = useSelector((state: RootState) => state.postsList);
 	const [searchValue, setSearchState] = useState('');
@@ -21,17 +30,28 @@ const Posts: React.FC = () => {
 	useEffect(() => {
 		dispatch(updatePostsRequest());
 	}, [dispatch]);
+	const [columnOrder, setColumnOrder] = useState(['id', 'title', 'body']);
+
+	const [columnWidths] = useState([
+		{ columnName: 'id', width: 100 },
+		{ columnName: 'title', width: 500 },
+		{ columnName: 'body', width: 800 },
+	]);
+
 	return (
-		<Paper>
+		<Paper variant="outlined">
 			<Grid rows={postsList} columns={columns}>
+				<DragDropProvider />
 				<SearchState value={searchValue} onValueChange={setSearchState} />
 				<IntegratedFiltering />
 				<SortingState defaultSorting={[{ columnName: 'id', direction: 'asc' }]} />
 				<IntegratedSorting />
 				<Table />
-				<TableHeaderRow showSortingControls/>
+				<TableColumnResizing columnWidths={columnWidths} />
+				<TableHeaderRow showSortingControls />
 				<Toolbar />
 				<SearchPanel />
+				<TableColumnReordering order={columnOrder} onOrderChange={setColumnOrder} />
 			</Grid>
 		</Paper>
 	);
